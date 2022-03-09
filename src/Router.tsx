2ffbb2e157c2e66e7b6fb50952a3d3a2787af9c5
연훈 배 <sbaru1997@naver.com>
2022-03-09 23:31:06 +0900

    feat: 로그인 페이지 템플릿 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
