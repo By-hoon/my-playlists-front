ba422b736e3a40e0ffa59d501a65e8f3d9ce9564
연훈 배 <sbaru1997@naver.com>
2022-03-14 03:56:29 +0900

    feat: login 여부에 따른 라우팅 제어

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,15 +3,25 @@ import Header from "./components/Header";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 
+import { isLoggedIn } from "./test/user";
+
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
+      {isLoggedIn ? (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </Routes>
+        </>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate replace to="/login" />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 };
