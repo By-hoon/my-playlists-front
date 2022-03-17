dbe7247a6a139116acce3eed897edd9bb62d7b48
연훈 배 <sbaru1997@naver.com>
2022-03-18 05:52:26 +0900

    feat: 플레이리스트 박스 컴포넌트 수정

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,25 +1,19 @@
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 import { PlaylistProp } from "../shared/Props";
-import tempThum from "../test/tempThum.jpg";
 
 const Playlist = (playlistObj: PlaylistProp) => {
   return (
-    <div className="playlist__container">
-      <Link
-        to={`/playlist/${playlistObj.playlistProps.playlistId}`}
-        className="link"
-      >
-        <img src={tempThum} alt="crew-logo" className="playlist__image" />
-        <div className="playlist__container--forward">
-          <span className="playlist__span--amount">
-            {playlistObj.playlistProps.songs.length}
-          </span>
-          <div className="playlist-icon__container">
-            <Icon icon="bxs:playlist" />
-          </div>
-        </div>
-        <div className="playlist__container--backward">
+    <Link
+      to={`/playlist`}
+      state={{
+        path: "/playlistDetail",
+        pid: `${playlistObj.playlistProps.playlistId}`,
+      }}
+      className="playlist__link"
+    >
+      <div className="playlist__container">
+        <div className="playlist__container--left">
           <span className="playlist__span--title">
             {playlistObj.playlistProps.title}
           </span>
@@ -30,8 +24,21 @@ const Playlist = (playlistObj: PlaylistProp) => {
             {playlistObj.playlistProps.updatedDate}
           </span>
         </div>
-      </Link>
-    </div>
+        <div className="playlist__container--right">
+          <span className="playlist__span--amount">
+            {playlistObj.playlistProps.songs.length}
+          </span>
+          <div className="playlist-icon__container">
+            <Icon icon="bxs:playlist" />
+          </div>
+        </div>
+        <div className="hidden-background__container">
+          <span className="hidden-background__containe__span">
+            <Icon icon="codicon:debug-start" />
+          </span>
+        </div>
+      </div>
+    </Link>
   );
 };
 
